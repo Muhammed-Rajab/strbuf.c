017aff7dbcec64996fded81a761819a03194fefa
Rajab-Kabeer <muhammedrajab101@gmail.com>
2026-02-03 13:30:08 +0530

    created a custom error enum. also created a function to get error as string. reserve method looking fine as hell.

--- include/strbuf/strbuf.h
+++ include/strbuf/strbuf.h
@@ -2,6 +2,14 @@
 #define STRBUF_H_
 
 #include <stddef.h>
+#include <stdlib.h>
+
+typedef enum {
+  STRBUF_OK = 0,
+  STRBUF_ERR_OOM,
+  STRBUF_ERR_RANGE,
+  STRBUF_ERR_INVALID,
+} strbuf_err;
 
 typedef struct {
   char *data;
@@ -14,5 +22,9 @@ void strbuf_free(strbuf *sb);
 
 size_t strbuf_len(const strbuf *sb);
 const char *strbuf_cstr(const strbuf *sb);
+
+const char *strbuf_errstr(strbuf_err err);
+
+strbuf_err strbuf_reserve(strbuf *sb, size_t needed);
 
 #endif
--- src/strbuf.c
+++ src/strbuf.c
@@ -28,3 +28,39 @@ void strbuf_free(strbuf *sb) {
   sb->cap = 0;
   sb->data = NULL;
 }
+
+size_t strbuf_len(const strbuf *sb) { return sb->len; }
+
+const char *strbuf_cstr(const strbuf *sb) { return sb->data; }
+
+strbuf_err strbuf_reserve(strbuf *sb, size_t needed) {
+  if (needed <= sb->cap)
+    return STRBUF_OK;
+
+  size_t new_cap = sb->cap;
+  while (new_cap < needed)
+    new_cap *= 2;
+
+  char *p = (char *)realloc(sb->data, new_cap);
+  if (!p)
+    return STRBUF_ERR_OOM;
+
+  sb->data = p;
+  sb->cap = new_cap;
+  return STRBUF_OK;
+}
+
+const char *strbuf_errstr(strbuf_err err) {
+  switch (err) {
+  case STRBUF_OK:
+    return "ok";
+  case STRBUF_ERR_OOM:
+    return "out of memory";
+  case STRBUF_ERR_INVALID:
+    return "invalid argument";
+  case STRBUF_ERR_RANGE:
+    return "out of range";
+  default:
+    return "unknown error";
+  }
+}
